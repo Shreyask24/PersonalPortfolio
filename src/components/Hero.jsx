0828bc728d416e52c684afff0878d8a5c74c8c4d
Shreyas-pgagi <shreyaskallurkar@pgagi.in>
2025-08-09 23:39:20 +0530

    JSON-LD integrate

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,11 @@
 import { motion } from "framer-motion"
-
 import { styles } from "../styles"
-import { ComputersCanvas } from "./canvas";
-
-import PersoalDesktop from "../assets/Personal-desktop.png"
-import { Tilt } from "react-tilt";
-import { SparklesPreview } from "./Sparkles";
 import { FlipWordsDemo } from "./TextGenerator";
 
 
 const Hero = () => {
   return (
-
-
     <section className="relative w-full h-[90vh] mx-auto">
-
       <div className="flex flex-col justify-center items-center w-full h-full">
         <h1 className={`${styles.heroHeadText} text-white text-center w-[70%]`}>Transforming Ideas into Successful <span className="text-[#915eff]"> Reality </span></h1>
 
@@ -22,35 +13,22 @@ const Hero = () => {
           <FlipWordsDemo />
         </div>
 
-        {/* <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]"> Shreyas Kallurkar </span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}> I build interactive user interfaces and scalable web apps.
-            <br className="sm:block hidden " /> Focused on efficiency and user experience.
-          </p> */}
-
-
+        {/* Hidden SEO keywords for crawlers */}
+        <span className="sr-only">
+          Frontend Developer, Full-Stack Developer, MERN Stack Developer, Modern Web Developer,
+          Freelance Web Developer, Node.js Enthusiast, MongoDB Explorer, Open Source Contributor
+        </span>
       </div>
-
-
-      <div className=" flex justify-center items-center ">
-
-        {/* <img className="my-80 hidden mt-[62vh] w-[90%] xl:block xl:mt-[48vh] lg:block lg:w-[30%]" src={PersoalDesktop} alt="" /> */}
-
-        {/* <SparklesPreview /> */}
-      </div>
-      {/* <ComputersCanvas /> */}
 
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex 
       justify-center items-center">
 
         <a href="#about">
-
-
           {/* The Scrolling Animation */}
 
           <div className="w-[35px] h-[64px] mb-[-20vh] rounded-3xl border-4
      border-secondary flex justify-center items-start p-2">
-
             <motion.div
               animate={{
                 y: [0, 24, 0]
@@ -64,18 +42,10 @@ const Hero = () => {
 
               className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
-
           </div>
-
         </a>
-
-
       </div>
     </section>
-
-
-
-
   )
 }
 
